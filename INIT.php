26d5a4cfee3eab1d92bae8af47677bc5ea3468d9
OihanOrabengoaLorea <oih_ora_lor@goierrieskola.org>
2026-02-09 11:30:32 +0100

    DB Konexio sortuta

--- INIT.php
+++ INIT.php
@@ -5,10 +5,11 @@ if (session_status() === PHP_SESSION_NONE) {
 
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
 
-$host = "localhost";
+$host = "192.168.115.171";
+$user = "erabiltzaile";
+$pass = "2TALDEA";
 $dbname = "erronka_2taldea";
-$user = "root";
-$pass = "1MG32025";
+
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
