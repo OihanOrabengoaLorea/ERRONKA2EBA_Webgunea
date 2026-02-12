e48a8cdc5d4783b5a233ed1545966c555701f99f
OihanOrabengoa <oih_ora_lor@goierrieskola.org>
2026-02-12 09:31:31 +0100

    Web-a komponduta

--- FAKTURA_DESKARGATU.php
+++ FAKTURA_DESKARGATU.php
@@ -20,8 +20,36 @@ if (!$faktura || empty($faktura['faktura_ruta'])) {
 
 $ruta_archivo = $faktura['faktura_ruta']; 
 
-if (file_exists($ruta_archivo)) {
-    
+// FTP server might map directly to the folder, so we check if we need to strip 'fakturak/' prefix
+$remote_path = $ruta_archivo;
+if (strpos(strtolower($remote_path), 'fakturak/') === 0) {
+    $remote_path = substr($remote_path, 9);
+}
+
+$ftp_server = "192.168.115.171";
+$ftp_user = "user1";
+$ftp_pass = "123";
+
+$conn_id = ftp_connect($ftp_server);
+if (!$conn_id) {
+    exit("Errorea: Ezin izan da FTP zerbitzariarekin konektatu.");
+}
+
+$login_result = ftp_login($conn_id, $ftp_user, $ftp_pass);
+if (!$login_result) {
+    ftp_close($conn_id);
+    exit("Errorea: FTP autentikazioak huts egin du.");
+}
+
+ftp_pasv($conn_id, true);
+
+$temp_handle = fopen('php://temp', 'r+');
+
+if (ftp_fget($conn_id, $temp_handle, $remote_path, FTP_BINARY)) {
+    rewind($temp_handle);
+    $fstat = fstat($temp_handle);
+    $size = $fstat['size'];
+
     if (ob_get_level()) {
         ob_end_clean();
     }
@@ -29,13 +57,16 @@ if (file_exists($ruta_archivo)) {
     header('Content-Description: File Transfer');
     header('Content-Type: application/pdf');
     header('Content-Disposition: attachment; filename="' . basename($ruta_archivo) . '"');
-    header('Content-Length: ' . filesize($ruta_archivo));
+    header('Content-Length: ' . $size);
     header('Pragma: public');
     
-    readfile($ruta_archivo);
-    
+    fpassthru($temp_handle);
+    fclose($temp_handle);
+    ftp_close($conn_id);
     exit;
 } else {
-    echo "Errorea: Fitxategia ez dago zerbitzarian.";
+    fclose($temp_handle);
+    ftp_close($conn_id);
+    echo "Errorea: Fitxategia ez da aurkitu zerbitzarian ($remote_path).";
 }
 ?>
